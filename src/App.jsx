53ef5e49ac4f478ee9760815b160f7b30e6f685d
David <davidpbarros96@gmail.com>
2025-08-04 15:21:15 -0300

    Feat: Ajustando componentes e implementado banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
-import styled from "styled-components"
-import EstilosGlobais from "./componentes/EstilosGlobais"
-import Cabecalho from "./componentes/Cabecalho"
-import BarraLateral from "./componentes/BarraLateral"
+import styled from "styled-components";
+import EstilosGlobais from "./componentes/EstilosGlobais";
+import Cabecalho from "./componentes/Cabecalho";
+import BarraLateral from "./componentes/BarraLateral";
+import Banner from "./componentes/Banner";
+import bannerBackground from "./assets/banner.png";
 
 const FundoGradiente = styled.div`
-  background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
+  background: linear-gradient(
+    174.61deg,
+    #041833 4.16%,
+    #04244f 48%,
+    #154580 96.76%
+  );
   width: 100%;
   min-height: 100vh;
-`
+`;
 function App() {
-
-
   return (
     <FundoGradiente>
       <EstilosGlobais />
       <Cabecalho />
       <BarraLateral />
+      <Banner
+        texto="A galeria mais completa do espaço"
+        backgroundImage={bannerBackground}
+      />
     </FundoGradiente>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/componentes/Cabecalho/index.jsx
+++ src/componentes/Cabecalho/index.jsx
@@ -1,23 +1,24 @@
-import { styled } from 'styled-components';
-import CampoTexto from '../../CampoTexto';
+import { styled } from "styled-components";
+import CampoTexto from "../../CampoTexto";
 
 const HeaderEstilizado = styled.header`
-    padding: 60px 0;
-    display: flex;
-    justify-content: space-between;
+  padding: 60px 0;
+  display: flex;
+  justify-content: space-between;
 
-    img {
-        max-width: 212px;
-    }
+  img {
+    max-width: 212px;
+  }
 `;
 
 const Cabecalho = () => {
-    return (<HeaderEstilizado>
-        <img src="imagens/logo.png" alt="Logo" />
-        <CampoTexto />
-    </HeaderEstilizado>
+  return (
+    <HeaderEstilizado>
+      <img src="imagens/logo.png" alt="Logo" />
+      <CampoTexto />
 
-    )
-}
+    </HeaderEstilizado>
+  );
+};
 
 export default Cabecalho;
