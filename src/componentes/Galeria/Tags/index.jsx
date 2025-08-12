7fd0301992a4dac990e27ec3841960195eb58b6f
David <davidpbarros96@gmail.com>
2025-08-12 16:59:51 -0300

    Feat: criada as tags e estilizada

--- src/componentes/Galeria/Tags/index.jsx
+++ src/componentes/Galeria/Tags/index.jsx
@@ -1,5 +1,48 @@
+import Titulo from "../../Titulo";
+import tags from "./tags.json";
+import styled from "styled-components";
+
 const Tags = () => {
-    return <p>Tags</p>
-}
+
+  const TagsEstializado = styled.section`
+    display: flex;
+    gap: 60px;
+    align-items: center;
+    color: #d9d9d9;
+    font-size: 24px;
+    line-height: 28.8px;
+    font-weight: 400;
+  `;
+
+  const TagTitulo = styled.h3`
+    color: #d9d9d9;
+    font-size: 24px;
+    margin: 0;
+  `;
+
+  const Tag = styled.button`
+    font-size: 24px;
+    color: #ffffff;
+    background: rgba(217, 217, 217, 0.3);
+    border-radius: 10px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+    padding: 12px;
+    box-sizing: border-box;
+    border: 2px solid transparent;
+    &:hover {
+      border-color: #c98cf1;
+    }
+  `;
+
+  return (
+    <TagsEstializado>
+      <Titulo>Busque por tags</Titulo>
+      {tags.map((tag) => (
+        <Tag key={tag.id}>{tag.titulo}</Tag>
+      ))}
+    </TagsEstializado>
+  );
+};
 
 export default Tags;
